f3df2f8270e235582fd465035933d67d49cb4b44
Ted M. Young <tedyoung@gmail.com>
2023-07-12 15:39:28 -0700

    LAB #4: Added characterization test for suit

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -8,6 +8,7 @@ public class Card {
     private final String suit;
     private final String rank;
 
+    // Card(Suit suit, String rank)
     public Card(String suit, String rank) {
         this.suit = suit;
         this.rank = rank;
