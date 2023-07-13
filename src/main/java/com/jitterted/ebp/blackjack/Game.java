6208e4573414283e338e08b44442b933d9cd5dbe
Ted M. Young <tedyoung@gmail.com>
2023-07-13 16:33:56 -0700

    BDD: F - player bets once (or many) and balance is reduced

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -203,4 +203,8 @@ public class Game {
     public int playerBalance() {
         return playerBalance;
     }
+
+    public void playerDeposits(int depositAmount) {
+        playerBalance += depositAmount;
+    }
 }
