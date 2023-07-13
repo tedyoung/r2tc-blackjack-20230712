8a53e58dfd5b57233dd72f16cb060372385785d5
Ted M. Young <tedyoung@gmail.com>
2023-07-13 16:32:06 -0700

    BDD: F - player balance starts at 0

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -13,6 +13,7 @@ public class Game {
 
     private final Hand playerHand = new Hand();
     private final Hand dealerHand = new Hand();
+    private int playerBalance = 0;
 
     public static void main(String[] args) {
         installAnsiDisplay();
@@ -199,4 +200,7 @@ public class Game {
                         .a("└─────────┘"));
     }
 
+    public int playerBalance() {
+        return playerBalance;
+    }
 }
